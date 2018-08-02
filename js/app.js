52ee4a477a9b62424aff89229b9cae1751351045
Marek <marekficht@gmail.com>
2018-08-02 17:29:30 +0200

    add moving for Furry :)

--- js/app.js
+++ js/app.js
@@ -25,7 +25,7 @@ function Game() {
         return x + (y * 10); // x: 0-9 right, y: 0-9 down.
     }
 
-    //--- show points on board
+    //--- show points(furry & coin) on board
     this.showFurry = function () {
         this.board[ this.index(this.furry.x, this.furry.y) ].classList.add('furry');
     }
@@ -33,11 +33,42 @@ function Game() {
     this.showCoin = function () {
         this.board[ this.index(this.coin.x, this.coin.y) ].classList.add('coin');
     }
+    
+    //--- hide points(furry & coin) on board
+    this.hideVisibleFurry = function () {
+        document.querySelector('.furry').removeAttribute('class');
+    }
+
+    //--- trick with 'this.' :)
+    var self = this;
+
+    //---
+    this.moveFurry = function () {
+        this.hideVisibleFurry();
+
+        if (this.furry.direction === 'right') {
+            this.furry.x = this.furry.x + 1;
+        }
+        else if (this.furry.direction === 'left'){
+            this.furry.x = this.furry.x - 1;
+        }
+        else if (this.furry.direction === 'bottom'){
+            this.furry.x = this.furry.y + 1;
+        }
+        else {
+            this.furry.x = this.furry.y - 1;
+        }
+
+        this.showFurry();
+    }
 
     //--- interval game
     this.startGame = function () {
         this.idSetIntercal = setInterval(function () {
-            console.log('text from setInterval.');
+            // console.log('text from setInterval.');
+
+            self.moveFurry();
+
         }, 250);
     }
 }
